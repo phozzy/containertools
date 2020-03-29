2a2283f4a5efca4b32eeed7d37a2918167db9299
phozzy <arthur.fayzullin@gmail.com>
2020-03-29 15:34:54 +0200

    Define cgroups

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-export $(podman run registry.fedoraproject.org/fedora grep VERSION_ID /etc/os-release)
+export $(podman --cgroup-manager=cgroupfs run registry.fedoraproject.org/fedora grep VERSION_ID /etc/os-release)
 echo ${VERSION_ID}
