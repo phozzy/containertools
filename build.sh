532507b8c0a681d97fa7508c03cdb177115d11bb
phozzy <arthur.fayzullin@gmail.com>
2020-03-29 13:08:14 +0200

    Fix podman command

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-export $(podman registry.fedoraproject.org/fedora grep VERSION_ID /etc/os-release)
+export $(podman run registry.fedoraproject.org/fedora grep VERSION_ID /etc/os-release)
 echo ${VERSION_ID}
